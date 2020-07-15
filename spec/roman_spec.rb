ccfec3642b74ba2a46a8334d65f354cce434ae23
Brian Ward <bward@vhtcx.com>
2020-07-15 11:34:05 -0400

    updated gems, added rake, updated specs

--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -1,43 +1,46 @@
 require_relative '../roman'
+require_relative 'spec_helper'
 
-describe Roman do
-  it 'should convert 1 to I' do
-    subject.convert(1).should == 'I'
-  end
-
-  it 'should convert 2 to II' do
-    subject.convert(2).should == 'II'
-  end
-
-  it 'should convert 3 to III' do
-    subject.convert(3).should == 'III'
-  end
-
-  it 'should convert 4 to IV' do
-    subject.convert(4).should == 'IV'
-  end
-
-  it 'should convert 5 to V' do
-    subject.convert(5).should == 'V'
-  end
-
-  it 'should convert 6 to VI' do
-    subject.convert(6).should == 'VI'
-  end
-
-  it 'should convert 9 to IX' do
-    subject.convert(9).should == 'IX'
-  end
-
-  it 'should convert 10 to X' do
-    subject.convert(10).should == 'X'
-  end
-
-  it 'should convert 11 to XI' do
-    subject.convert(11).should == 'XI'
-  end
-
-  it 'should convert 38 to XXXVIII' do
-    subject.convert(38).should == 'XXXVIII'
+RSpec.describe Roman do
+  describe '#convert' do
+    it 'should convert 1 to I' do
+      expect(subject.convert(1)).to  eq('I')
+    end
+    
+    it 'should convert 2 to II' do
+      expect(subject.convert(2)).to eq('II')
+    end
+    
+    it 'should convert 3 to III' do
+      expect(subject.convert(3)).to eq('III')
+    end
+    
+    it 'should convert 4 to IV' do
+      expect(subject.convert(4)).to eq('IV')
+    end
+    
+    it 'should convert 5 to V' do
+      expect(subject.convert(5)).to eq('V')
+    end
+    
+    it 'should convert 6 to VI' do
+      expect(subject.convert(6)).to eq('VI')
+    end
+    
+    it 'should convert 9 to IX' do
+      expect(subject.convert(9)).to eq('IX')
+    end
+    
+    it 'should convert 10 to X' do
+      expect(subject.convert(10)).to eq('X')
+    end
+    
+    it 'should convert 11 to XI' do
+      expect(subject.convert(11)).to eq('XI')
+    end
+    
+    it 'should convert 38 to XXXVIII' do
+      expect(subject.convert(38)).to eq('XXXVIII')
+    end
   end
 end
